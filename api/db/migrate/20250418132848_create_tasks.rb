9af2dee4c7379abef4f92ec95b02528d2ff899df
kenwoo9y <kenwoo9y@gmail.com>
2025-04-18 22:29:26 +0900

    create task model

---
+++ api/db/migrate/20250418132848_create_tasks.rb
@@ -0,0 +1,13 @@
+class CreateTasks < ActiveRecord::Migration[7.1]
+  def change
+    create_table :tasks do |t|
+      t.string :title, limit: 30
+      t.string :description, limit: 255
+      t.date :due_date
+      t.string :status, limit: 10
+      t.references :owner, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
