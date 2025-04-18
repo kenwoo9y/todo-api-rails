a374b15e323cc2157e9fcd36a88ffd67c7669979
kenwoo9y <kenwoo9y@gmail.com>
2025-04-18 22:39:51 +0900

    fix tasks references

--- api/db/migrate/20250418132848_create_tasks.rb
+++ api/db/migrate/20250418132848_create_tasks.rb
@@ -5,7 +5,7 @@ class CreateTasks < ActiveRecord::Migration[7.1]
       t.string :description, limit: 255
       t.date :due_date
       t.string :status, limit: 10
-      t.references :owner, null: false, foreign_key: true
+      t.references :owner, null: true, foreign_key: { to_table: :users }
 
       t.timestamps
     end
