2872e974b41584d5897acd66f81f9fded9dde3b5
kenwoo9y <kenwoo9y@gmail.com>
2025-04-19 10:50:56 +0900

    fix user model

--- api/app/models/user.rb
+++ api/app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ApplicationRecord
+    # Associated with Task model
     has_many :tasks, foreign_key: :owner_id, dependent: :destroy
+
+    # Validations
+    validates :username, presence: true, uniqueness: true, length: { maximum: 30 }
+    validates :email, presence: true, uniqueness: true, length: { maximum: 80 }, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :first_name, length: { maximum: 40 }, allow_blank: true
+    validates :last_name,  length: { maximum: 40 }, allow_blank: true
 end
