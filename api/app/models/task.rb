52de0d69c3eecb83a3e31f49d431d750f87078f8
kenwoo9y <kenwoo9y@gmail.com>
2025-04-19 10:51:12 +0900

    fix task model

--- api/app/models/task.rb
+++ api/app/models/task.rb
@@ -1,3 +1,19 @@
 class Task < ApplicationRecord
+  # Associated with User model
   belongs_to :owner, class_name: "User"
+  
+  # Validations
+  validates :title, presence: true, length: { maximum: 30 }
+  validates :description, length: { maximum: 255 }, allow_blank: true
+  validates :status, inclusion: { in: STATUSES.keys }, allow_blank: true
+  validates :due_date, allow_blank: true
+
+  # Status
+  STATUSES = {
+    "ToDo" => "ToDo",
+    "Doing" => "Doing",
+    "Done" => "Done"
+  }.freeze
+
+  enum status: STATUSES, _prefix: :status
 end
