1d68d62b20c16722b63a3c97a654dc72292c142d
kenwoo9y <kenwoo9y@gmail.com>
2025-04-20 12:34:04 +0900

    rails generate tasks controller

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'tasks/index'
+  get 'tasks/show'
+  get 'tasks/create'
+  get 'tasks/update'
+  get 'tasks/destroy'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
