7d660ae2ba694f98b0bc8eb5690643992c81eb84
kenwoo9y <kenwoo9y@gmail.com>
2025-04-20 12:58:15 +0900

    Implemented the tasks controller

--- api/app/controllers/tasks_controller.rb
+++ api/app/controllers/tasks_controller.rb
@@ -1,16 +1,51 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :update, :destroy]
+
+  # GET /tasks
   def index
+    @tasks = Task.all
+    render json: @tasks
   end
 
+  # GET /tasks/:id
   def show
+    render json: @task
   end
 
+  # POST /tasks
   def create
+    @task = Task.new(task_params)
+    if @task.save
+      render json: @task, status: :created
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
+  # PATCH /tasks/:id
   def update
+    if @task.update(task_params)
+      render json: @task
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
+  # DELETE /tasks/:id
   def destroy
+    @task.destroy
+    head :no_content
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render json: { error: "Task not found" }, status: :not_found
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :description, :status, :due_date, :owner_id)
   end
 end
--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'tasks/index'
-  get 'tasks/show'
-  get 'tasks/create'
-  get 'tasks/update'
-  get 'tasks/destroy'
+  # Tasks
+  resources :tasks
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
